fea3c13adc43cb7724c4e790572b3f49f40baa28
Breismam <baruedad@upn.edu.co>
2022-07-05 22:43:05 -0500

    Login sin BD

--- Index.js
+++ Index.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {config} = require('./Src/Config/index');
+const { config } = require("./Src/Config/index");
+var bodyparser = require('body-parser'); //body-parser es requerido para descomponer el request
 
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static("./Src/Public"));
+app.get("/", (req, res) => res.send("Hola Pagina principal"));
+app.get("/Users", (req, res) => res.send("Login"));
+app.get("/Devices", (req, res) => res.send("Inventario"));
 
-app.get('/', (req,res) => res.send('Hola mundo'))
-
-app.listen(config.port, () =>{
-    console.log(`Servidor escuchando en el puerto ${config.port}`);
+app.post("/guardar", (req, res) => {
+    console.log("el archivo fue guardado");
+    console.log(`El nombre del usurio es: ${req.body.user} y la contraseña es: ${req.body.pass}`);
 });
+
+app.listen(config.port, () => {
+  console.log(`Servidor escuchando en el puerto ${config.port}`);
+});  
